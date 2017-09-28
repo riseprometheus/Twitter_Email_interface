d8c8d5717451f4c381777183f7721b5b95ec3f9f
Chris Bilbo <ChristopherBilbo@gmail.com>
2017-09-28 17:31:09 -0400

    Fixed issue where closing manual tweet window broke a button

--- Main.py
+++ Main.py
@@ -4,59 +4,55 @@ import tkinter
 import tweepy
 
 class guiWindow(tkinter.Frame):
-    manualTweetOpenFlag=False
+    manualTweetOpenFlag = False
     def __init__(self, master=None):
         super().__init__(master)
-        #move into function that will be called in constructor 
-        
-        master.LFrame = tkinter.LabelFrame(master,text="Twitter/Email Interface",
-                    padx=20, pady=20,relief='sunken')
-        master.LFrame.grid(column=1)
-        
-        
-        
-        master.l1 = tkinter.Label(master.LFrame,text="What would you like to do?")
+        #move into function that will be called in constructor
+        master.LFrame = tkinter.LabelFrame(master, text="Twitter/Email Interface",
+                                           padx=15, pady=15,relief='sunken')
+        master.LFrame.grid(row=0,columnspan=2,rowspan=2, padx=5, pady=5,stick=NS)
+        master.l1 = tkinter.Label(master.LFrame, text="What would you like to do?")
         master.l1.grid(row=1, column=1)
-        
-        
-        master.tweetButton = tkinter.Button(master.LFrame,text="Send Manual Tweet",command=self.create_widgets)
+        master.tweetButton = tkinter.Button(master.LFrame, text="Send Manual Tweet",
+                                            command = lambda: self.create_widgets(master))
         master.tweetButton.grid(row=2, column=1)
-        
-
-    def create_widgets(self):
+    def create_widgets(self,master):
         if self.manualTweetOpenFlag:
             return
-        self.manualTweetOpenFlag=True
-        
-        top = tkinter.Toplevel(self)
+        self.manualTweetOpenFlag = True
+        top = tkinter.Toplevel(master)
         top.title('Send Manual Tweet')
-        top.l1 = tkinter.Label(top,text="What would you like to tweet?")
+        top.protocol('WM_DELETE_WINDOW',lambda: self.closeManualTweetWindow(top))
+
+        top.l1 = tkinter.Label(top, text="What would you like to tweet?")
         top.l1.grid(row=0)
         top.t1 = tkinter.Text(top)
         top.t1.grid(row=1)
 
-        top.b1 = tkinter.Button(top,text="Submit",command=lambda:self.submit(top))
+        top.b1 = tkinter.Button(top, text="Submit", command=lambda: self.submit(top))
         top.b1.grid(row=2)
-
-    def submit(self,topLevel):
-        tweet=topLevel.t1.get('1.0',tkinter.END)
-        topLevel.t1.delete('1.0',tkinter.END)
+    def submit(self, topLevel):
+        tweet = topLevel.t1.get('1.0', tkinter.END)
+        topLevel.t1.delete('1.0', tkinter.END)
         print(tweet)
 
         if not (tweet and tweet.strip()):
-            messagebox.showwarning("Warning","There is nothing to submit. Try Again.",parent=topLevel)
+            messagebox.showwarning("Warning", "There is nothing to submit. Try Again.",
+                                   parent=topLevel)
             return
-        if len(tweet)>140:
-            messagebox.showwarning("Warning","Your current tweet is too long. Please shorten.",parent=topLevel)
+        if len(tweet) > 140:
+            messagebox.showwarning("Warning", "Your current tweet is too long. Please shorten.",
+                                   parent=topLevel)
             return
 
-        messagebox.showinfo("Success", "Text Captured.",parent=topLevel)
+        messagebox.showinfo("Success", "Text Captured.", parent=topLevel)
         #implement tweet mechanism when oath tokens are set up
         topLevel.destroy()
+        self.manualTweetOpenFlag = False
+    def closeManualTweetWindow(self, topLevel):
         self.manualTweetOpenFlag=False
-
+        topLevel.destroy()
 root = tkinter.Tk()
 root.title('Root Window')
-
 app = guiWindow(master=root)
 root.mainloop()
