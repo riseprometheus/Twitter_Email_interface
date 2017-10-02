0f5d5ea81491fd3cba277094a31cec5ace742723
Chris Bilbo <ChristopherBilbo@gmail.com>
2017-10-02 16:36:19 -0400

    added New field on main screen for recent tweets

--- Main.py
+++ Main.py
@@ -5,26 +5,36 @@ import tweepy
 
 class guiWindow(tkinter.Frame):
     manualTweetOpenFlag = False
+    lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     def __init__(self, master=None):
         super().__init__(master)
         #move into function that will be called in constructor
         master.LFrame = tkinter.LabelFrame(master, text="Twitter/Email Interface",
                                            padx=15, pady=15,relief='sunken')
-        master.LFrame.grid(row=0,columnspan=2,rowspan=2, padx=5, pady=5,stick=NS)
+        master.LFrame.grid(row=0, column=0, columnspan=2,rowspan=2, padx=5,pady=5,stick=NS)
         master.l1 = tkinter.Label(master.LFrame, text="What would you like to do?")
-        master.l1.grid(row=1, column=1)
+        master.l1.grid(row=1, column=0)
         master.tweetButton = tkinter.Button(master.LFrame, text="Send Manual Tweet",
                                             command = lambda: self.create_widgets(master))
-        master.tweetButton.grid(row=2, column=1)
+        master.tweetButton.grid(row=2, column=0)
 
         master.LFrame2 = tkinter.LabelFrame(master, text = "Import New Submissions",
-                                            padx=15, pady=15,relief='sunken')
+                                            padx=15,relief='sunken')
         master.LFrame2.grid(row=3,columnspan=2,rowspan=2, padx=5, pady=5,stick=NS)
         master.l2 = tkinter.Label(master.LFrame2, text="What would you like to do?")
         master.l2.grid(row=4, column=1)
         master.importButtons = tkinter.Button(master.LFrame2, text="Import",
                                             command = lambda: self.dummyButton(master))
         master.importButtons.grid(row=5, column=1)
+
+        master.LFrame3 = tkinter.LabelFrame(master, text="Most recent Tweets",
+                                           padx=15, relief='sunken')
+        master.LFrame3.grid(row=0, column=2, columnspan=4,rowspan=2, padx=5,pady=5,stick=NS)
+
+        master.l3 = tkinter.Label(master.LFrame3, text="Here are your most recent tweets.")
+        master.l3.grid(row=1, column=2)
+        master.recentTweets = tkinter.Label(master.LFrame3,text=self.lorem)
+        master.recentTweets.grid(row=2, column=2)
 
 
     def create_widgets(self,master):
